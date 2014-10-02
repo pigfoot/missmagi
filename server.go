7b96f34da641861fec38a590efd4b824c2bb49a0
Chen, Chih-Chia <pigfoot@gmail.com>
2014-10-02 19:07:05 +0800

    Add bootstrap based templates.

--- server.go
+++ server.go
@@ -1,15 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	gin.SetMode("release")
+
 	r := gin.Default()
-	r.LoadHTMLTemplates("templates/*")
+	r.LoadHTMLGlob("templates/*.tmpl")
+	r.Static("/assets", "assets")
+
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(301, "/einvoice")
+	})
 
 	r.GET("/einvoice", func(c *gin.Context) {
-		c.String(200, "einvoice.tmpl")
+		obj := gin.H{"PageTitle": "E-invoice"}
+		c.HTML(200, "einvoice", obj)
 	})
 
 	// Listen and server on 0.0.0.0:8080
-	r.Run(":80")
+	port := "80"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	r.Run(":" + port)
 }
